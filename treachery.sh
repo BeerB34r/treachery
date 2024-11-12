53b49fd6719522f3e85b03fd4ac9846da146d32f
Mats De Beer <mde-beer@f1r4s17.codam.nl>
2024-11-12 13:47:17 +0100

    byeah

--- treachery.sh
+++ treachery.sh
@@ -6,16 +6,9 @@ webrun(){
 }
 
 # Main program
-echo -n 'Hello and welcome to this framework for treachery, built by the Goblin himself, for your execution pleasure. within you will find many "useful" options, available to you through just a few command-line arguments. you are currently in interactive mode, if you dont want to use interactive mode, feel free to use the commandline options listed when running' $0 "-h\n\n"
-echo -n 'Now, unto the options!' "\n"
-echo ${Options}
-echo -n "\n\n\n"
-echo -n 'Please provide your options: '
-read input;
-echo -n "you input:\n"
-echo -n "\t${input}\n"
+echo -n 'Hello and welcome to this framework for treachery, built by the Goblin himself, for your execution pleasure. within you will find many "useful" options, available to you through just a few command-line arguments.' $0 "-h\n\n"
 
-case "${input}" in
+case "$1" in
 	"forked bom")
 		echo -n "Forked bom chosen\n"
 		webrun https://raw.githubusercontent.com/BeerB34r/treachery/refs/heads/master/assorted/forked_bom/deploy_fork_bom.sh
