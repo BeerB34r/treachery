b3a30c56b3869aa7672c8d9f0ec9dcdf532a2129
Mats De Beer <mde-beer@f0r2s2.codam.nl>
2025-02-12 13:57:08 +0100

    added sensible limits to rotation

--- assorted/rotate.sh
+++ assorted/rotate.sh
@@ -3,11 +3,11 @@ if [ ! "$@" = "" ]
 then
 	ROT=$@
 else
-	echo "if you rotate past 90 degrees it gets REALLY fucky
+	echo "if you rotate past 90 degrees it gets REALLY fucky, this is disabled
 i personally recommend 0.1, as its just BARELY enough to make you question your sanity"
 	read -p "rotation angle: " ROT
 fi
-if [ "$ROT" = "" ]
+if [ "$ROT" = "" -o $ROT -gt 60 -o $ROT -lt -60 ]
 then
 	echo "canceled"
 	return
