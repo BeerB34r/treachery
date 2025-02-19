3b9b2e8ae2c9d8b66c112c65ca88a5d47ec6745a
Mats De Beer <mde-beer@f0r3s5.codam.nl>
2025-02-19 16:42:11 +0100

    yip!

--- assorted/theme.sh
+++ assorted/theme.sh
@@ -5,7 +5,9 @@ echo ${CURRENT}
 if [ "${CURRENT}" = "'Adwaita'" ]
 then
 	gsettings set org.gnome.desktop.interface gtk-theme "'Adwaita-dark'"
+	gsettings set org.gnome.desktop.interface color-scheme "'prefer-dark'"
 else
 	gsettings set org.gnome.desktop.interface gtk-theme "'Adwaita'"
+	gsettings set org.gnome.desktop.interface color-scheme "'prefer-light'"
 fi
 clear
